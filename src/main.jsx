d5561e69b1eabe17e44e3bb76e8d9bffd9a3d603
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-28 17:17:16 +0700

    Refactor: move createBrowserRouter to component App

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />
-    }
-  ]
-)
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App />
 )
