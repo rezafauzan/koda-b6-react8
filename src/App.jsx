3ce40efec3d6d02ef0b6a9af91483b8f8a812b0a
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-28 17:18:20 +0700

    Feat: add CredentialsContext to use in Login Page and Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import { RiLockPasswordFill } from "react-icons/ri"; 
-import { AiOutlineMail } from "react-icons/ai"; 
+import { createContext, useState } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+
+export const CredentialsContext = createContext()
 
 function App() {
-    
+    const [user, setUser] = useState()
+
+    const router = createBrowserRouter(
+        [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/login',
+                element: <Login />
+            }
+        ]
+    )
     return (
-        <div className="w-xl mx-auto bg-white rounded p-4 m-4">
-            <form className="flex w-full flex-col justify-center gap-4 p-4 flex-3" >
-                        <h1 className="font-bold text-(--color-accent)">Login</h1>
-                        <label htmlFor="email" className="font-bold text-black">Email</label>
-                        <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                            <AiOutlineMail />
-                            <input type="email" {...register("email")} id="email" placeholder="Enter your email" className="flex-1 outline-0" />
-                        </label>
-                        <label htmlFor="email" className="font-bold text-black">Password</label>
-                        <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                            <RiLockPasswordFill />
-                            <input type="password" {...register("password")} id="password" placeholder="Enter your password" className="flex-1 outline-0" />
-                        </label>
-                        <button className="bg-sky-700 text-black p-4 rounded cursor-pointer">Login</button>
-                        
-                    </form>
-        </div>
+        <CredentialsContext.Provider value={{user, setUser}} >
+            <RouterProvider router={router} />
+        </CredentialsContext.Provider >
     )
 }
 
