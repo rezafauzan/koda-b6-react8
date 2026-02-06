40581e6e2d9434ac478deb87ef0fc0de1c1050fe
rezafauzan <rezafauzan593@gmail.com>
2026-02-06 07:45:36 +0700

    Feat: change login method to set credentials directly to localstorage but still using useContext filled with user in localStorage; can't access login page when key user in local storage filled with right email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
-import { RiLockPasswordFill } from "react-icons/ri";
-import { AiOutlineMail } from "react-icons/ai";
-import { useForm } from "react-hook-form";
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { CredentialsContext } from "../App";
+import { RiLockPasswordFill } from "react-icons/ri"
+import { AiOutlineMail } from "react-icons/ai"
+import { useForm } from "react-hook-form"
+import { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { CredentialsContext } from "../App"
+import useLocalStorage from "/src/lib/useLocalStorage"
 
 export default function Login() {
     const credentials = useContext(CredentialsContext)
@@ -12,13 +13,21 @@ export default function Login() {
     const navigator = useNavigate()
     const submitHandler = data => {
         if (data.email === 'koda@email.com' && data.password === '1234') {
-            credentials.setUser(data)
+            credentials.setUser(data.email)
             setLoginError(false)
             navigator("/")
         } else {
             setLoginError(true)
         }
     }
+    useEffect(
+        ()=>{
+            console.log(credentials)
+            if(credentials.user.length > 0){
+                navigator("/")
+            }
+        },[]
+    )
     return (
         <div className="w-xl mx-auto bg-white rounded p-4 m-4">
             <form className="flex w-full flex-col justify-center gap-4 p-4 flex-3" onSubmit={handleSubmit(submitHandler)}>
